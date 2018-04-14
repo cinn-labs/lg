46273f1275a59429ff111c3009fc75976ba73df9
Chris Storch <ccstorch@gmail.com>
2018-04-14 17:37:09 +0100

    Up

--- lg.go
+++ lg.go
@@ -5,34 +5,35 @@ import (
 	"runtime"
 )
 
-func GetTrace() string {
+func GetTrace(level int) string {
 	pc := make([]uintptr, 10) // at least 1 entry needed
-	runtime.Callers(3, pc)
+	runtime.Callers(3+level, pc)
 	f := runtime.FuncForPC(pc[0])
 	return f.Name()
 }
 
 func Error(err error, data ...interface{}) {
-	fn := GetTrace()
+	fn := GetTrace(0)
 	p := append([]interface{}{"********ERROR ON: ", fn, " - ", err, " | "}, data...)
 	log.Println(p...)
 }
 
 func CError(err error, data ...interface{}) bool {
 	if err != nil {
-		Error(err, data...)
-		return true
+		fn := GetTrace(1)
+		p := append([]interface{}{"********ERROR ON: ", fn, " - ", err, " | "}, data...)
+		log.Println(p...)
 	}
 	return false
 }
 
 func ValidationError(v interface{}, err error) {
-	fn := GetTrace()
+	fn := GetTrace(0)
 	log.Println("*VALIDATION ON: ", fn, " - ", err, " | ", v)
 }
 
 func Info(data ...interface{}) {
-	fn := GetTrace()
+	fn := GetTrace(0)
 	p := append([]interface{}{"**INFO: ", fn, " - "}, data...)
 	log.Println(p...)
 }
